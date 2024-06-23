a829b1b6eeef68818ab5e4cdbb1bac8944d4345e
Christian Hase <hase.christian92@gmail.com>
2024-06-23 16:33:18 +0200

    hopefully working for secret files

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,18 @@
 #!/bin/sh -l
 
 COMPOSE_PATH="$1"
+COMPOSE_SECRET_PATH=$(echo $COMPOSE_PATH | sed  "s/docker-compose.yml/${INPUT_SECRET_FILENAME:-docker-compose.secret.yml}/")
 
 if [[ ! -f "$COMPOSE_PATH" ]]; then
     echo "$COMPOSE_PATH not found"
     exit 1
 fi
 
-echo checking "$COMPOSE_PATH"
-docker compose -f "$COMPOSE_PATH" config
+
+if [[ -f "$COMPOSE_SECRET_PATH" ]]; then
+    echo "Secret File found. Using both files for linting."
+    docker compose -f "$COMPOSE_PATH" -f "$COMPOSE_SECRET_PATH" config
+else
+    echo checking "$COMPOSE_PATH"
+    docker compose -f "$COMPOSE_PATH" config
+fi
